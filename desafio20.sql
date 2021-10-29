2003ef19d17366a9eff8f56fdbac7cee8793733c
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-28 23:28:28 -0300

    faz o 20

---
+++ desafio20.sql
@@ -0,0 +1,15 @@
+USE hr; 
+DELIMITER $$
+
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(50)) 
+BEGIN 
+SELECT CONCAT(em.FIRST_NAME, ' ', em.LAST_NAME) AS "Nome completo", de.DEPARTMENT_NAME AS "Departamento" , jb.JOB_TITLE AS  "Cargo" FROM job_history AS jh
+INNER JOIN employees AS em
+ON jh.EMPLOYEE_ID = em.EMPLOYEE_ID
+INNER JOIN departments AS de
+ON jh.DEPARTMENT_ID = de.DEPARTMENT_ID
+INNER JOIN jobs AS jb
+ON jh.JOB_ID = jb.JOB_ID
+WHERE em.EMAIL = email; 
+END $$
+DELIMITER ;
