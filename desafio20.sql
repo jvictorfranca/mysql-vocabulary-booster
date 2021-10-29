217dca4c62370727e417da6f5606890b4532dd5c
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-28 23:52:03 -0300

    tenta resolver o 20 de novo

--- desafio20.sql
+++ desafio20.sql
@@ -1,6 +1,6 @@
 USE hr; 
-DELIMITER $$
 
+DELIMITER $$
 CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(50)) 
 BEGIN 
 SELECT CONCAT(em.FIRST_NAME, ' ', em.LAST_NAME) AS "Nome completo", de.DEPARTMENT_NAME AS "Departamento" , jb.JOB_TITLE AS  "Cargo" FROM job_history AS jh
@@ -11,5 +11,6 @@ ON jh.DEPARTMENT_ID = de.DEPARTMENT_ID
 INNER JOIN jobs AS jb
 ON jh.JOB_ID = jb.JOB_ID
 WHERE em.EMAIL = email
+ORDER BY `Departamento`, `Cargo`; 
 END $$
 DELIMITER ;
